e2fd42bba0f1842f4bf6e7b574765e3cf83c7f0b
Frank Lyder Bredland <fl@nyggen.com>
2019-07-17 19:48:48 +0200

    Beginning of bootstrap setup

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,12 +1,19 @@
 import { dom } from 'isomorphic-jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './cover.css';
 
 const page = ({ children }) => '<!DOCTYPE html>' +
-	<html>
+	<html lang="en">
 		<head>
-			<title>BeerJS</title>
 			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+			<meta name="description" content="" />
+
+			<title>BeerJS</title>
+
+			<link rel="stylesheet" href="main.css" />
 		</head>
-		<body>{children}</body>
+		<body class="text-center">{children}</body>
 	</html>;
 
 export default page;
--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,40 @@ import { dom } from 'isomorphic-jsx';
 import Logo from './components/logo';
 import Layout from './components/layout';
 
+const Header = () => <header class="masthead mb-auto">
+	<div class="inner">
+		<h3 class="masthead-brand">
+			<Logo style={{ height: '2em' }} />
+			<span>BeerJs</span>
+		</h3>
+		<nav class="nav nav-masthead justify-content-center">
+			<a class="nav-link active" href="#">Home</a>
+			<a class="nav-link" href="blog/">Blog</a>
+			<a class="nav-link" href="locations/">Locations</a>
+		</nav>
+	</div>
+</header>;
+
+const Footer = () => <footer class="mastfoot mt-auto">
+	<div class="inner">
+		<p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+	</div>
+</footer>;
+
 const page = () => <Layout>
-	<h1> BeerJS </h1>
-	<Logo style={{ width: '200px' }} />
-	<a href="blog/">Blog</a>
-	<a href="locations/">Locations</a>
-</Layout>
+	<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+		<Header />
+
+		<main role="main" class="inner cover">
+			<h1 class="cover-heading">Cover title</h1>
+			<p class="lead">Here goes some text about beerjs</p>
+			<p class="lead">
+				<a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+			</p>
+		</main>
+
+		<Footer />
+	</div>
+</Layout>;
 
 export default page;
