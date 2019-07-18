f9a12197058f81bd0ca4baa98f5cfbc3e32e229e
Frank Lyder Bredland <fl@nyggen.com>
2019-07-18 23:31:57 +0200

    Added credit for cover photo

--- src/index.jsx
+++ src/index.jsx
@@ -23,7 +23,7 @@ const Header = () => <header class="masthead mb-auto">
 
 const Footer = () => <footer class="mastfoot mt-auto">
 	<div class="inner">
-		<p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+		<p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>. Photo by <a href="https://unsplash.com/@brookecagle">Brooke Cagle</a> on Unsplash.</p>
 	</div>
 </footer>;
 
