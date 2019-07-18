2d1137e720afe8e17c486f6f142c805b22ec54a5
Frank Lyder Bredland <fl@nyggen.com>
2019-07-19 00:56:37 +0200

    Relative url fix

--- src/index.jsx
+++ src/index.jsx
@@ -8,14 +8,14 @@ import coverImage from './assets/cover.jpeg';
 import Header from './components/header';
 import Footer from './components/footer';
 
-export default () => <Layout>
+export default () => <Layout relativeLocation='.'>
 	<body class="text-center" style={{
 		'background-image': `url(${coverImage})`,
 		'background-repeat': 'no-repeat',
 		'background-size': 'cover'
 	}}>
 		<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-			<Header />
+			<Header relativeLocation='.' />
 
 			<main role="main" class="inner cover">
 				<h1 class="cover-heading">Cover title</h1>
